94a3c8d4d973e230f7a33baf9aefee37bdcda991
Guangkai Tang <gktang46@gmail.com>
2026-03-17 01:17:31 -0400

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -98,8 +98,6 @@ def plot_covid_chart():
                                  stepmode="backward"),
                             dict(count=6, label="6m", step="month",
                                  stepmode="backward"),
-                            dict(count=1, label="YTD",
-                                 step="year", stepmode="todate"),
                             dict(count=1, label="1y", step="year",
                                  stepmode="backward"),
                             dict(step="all"),
